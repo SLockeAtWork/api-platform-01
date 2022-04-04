80eb191f18ead91dae3495258a5f48a241435b2f
SLocke <slocke@go2boss.com>
2022-04-04 13:57:02 -0400

    Git is very poopy.

---
+++ api/src/Entity/Review.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ReviewRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ReviewRepository::class)]
+#[ApiResource]
+class Review
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+	#[Assert\Range(min: 0, max: 5)]
+    private $rating;
+
+    #[ORM\Column(type: 'string', length: 255)]
+	#[Assert\NotBlank]
+    private $body;
+
+    #[ORM\Column(type: 'string', length: 255)]
+	#[Assert\NotBlank]
+    private $author;
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private $publicationDate;
+
+    #[ORM\ManyToOne(targetEntity: Book::class, inversedBy: 'reviews')]
+    private $book;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(?int $rating): self
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPublicationDate(): ?\DateTimeInterface
+    {
+        return $this->publicationDate;
+    }
+
+    public function setPublicationDate(?\DateTimeInterface $publicationDate): self
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    public function getBook(): ?Book
+    {
+        return $this->book;
+    }
+
+    public function setBook(?Book $book): self
+    {
+        $this->book = $book;
+
+        return $this;
+    }
+}
